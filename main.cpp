342b0089d290fa8be85f972cb96d2c6c575b68c7
Alexey Dorofeev <braen.vfx@gmail.com>
2023-04-06 11:02:51 +0300

    Chat

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "Chat.h"
+
+int main()
+{
+    Chat chat;
+	chat.start();
+
+	while (chat.isChatWork())
+	{
+		chat.showLoginMenu();
+
+		while (chat.getCurrentUser())
+		{
+			chat.showUserMenu();
+		}
+
+	}
+    
+}
+
